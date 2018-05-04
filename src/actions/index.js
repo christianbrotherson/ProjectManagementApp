61fd6accc09a5d514b82e760dd5ed5179502a0cd
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-04 13:39:25 -0600

    setup action and get request to newsletter archive

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { FETCH_NEWSLETTER_ARCHIVE } from './types';
+
 import axios from 'axios';
 import history from '../history';
 
@@ -17,3 +19,18 @@ export function signinUser({email, password}) {
         })
     }
 }
+
+export function fetchNewsletterArchive() {
+    return function(dispatch) {
+        axios.get(`${ROOT_URL}/newsletterArchive`, {
+            headers: { authorization: localStorage.getItem('token')}
+        })
+            .then(response => {
+                dispatch({
+                    type: FETCH_NEWSLETTER_ARCHIVE,
+                    payload: response.data
+                })
+            })
+    }
+}
+
