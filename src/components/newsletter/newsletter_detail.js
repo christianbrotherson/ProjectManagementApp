089d6b36acd1a775d80aa29a4be64a0b2aa8fe1b
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-07 13:14:11 -0600

    rendered fetched item on the screen

--- src/components/newsletter/newsletter_detail.js
+++ src/components/newsletter/newsletter_detail.js
@@ -8,19 +8,40 @@ class NewsletterDetail extends Component {
         this.props.fetchNewsletterById(this.props.match.params._id)
     }
 
+    renderFetchedItem() {
+        if (this.props.fetchedItem) {
+            return (
+                <div>
+                    <div>
+                        <img src={this.props.fetchedItem.imageUrl} />
+                        <h2>{this.props.fetchedItem.title}</h2>
+                    </div>
+                    <p>
+                        {this.props.fetchedItem.body}
+                    </p>
+                </div>
+            )
+        }
+    }
+
     render() {
 
         return (
-            <div>
-                newsletter detail
+            <div className="row">
+                <div className="col-md-3">
+                    <div style={{ width: '200px', height: '200px', backgroundColor: 'skyblue' }}>
+                        02 jan 18
+                    </div>
+                </div>
+                <div className="col-md-9">
+                    {this.renderFetchedItem()}
+                </div>
             </div>
         )
     }
 }
 
-function mapStateToProps(state) {
-    console.log(state.newsletter.fetchedItem);
-        
+function mapStateToProps(state) {        
     return {
         fetchedItem: state.newsletter.fetchedItem
     }
