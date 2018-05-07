e6701bf333b968a871a5d7a2ec784db2992ab33d
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-07 13:32:56 -0600

    implemented route, button, and edit newsletter component

---
+++ src/components/newsletter/newsletter_edit.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EditNewsletter extends Component {
+    
+    render() {
+        return (
+            <form>
+                Form For Edit
+            </form>
+        );
+    }
+}
+
+export default EditNewsletter;
