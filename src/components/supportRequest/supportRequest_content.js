6299825431adb893776f369856c6e878d9089685
Christian Brotherson <christian.brotherson@gmail.com>
2018-05-08 15:24:31 -0600

    developed the support request item component

--- src/components/supportRequest/supportRequest_content.js
+++ src/components/supportRequest/supportRequest_content.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
+import SupportRequestItem from './supportRequest_item';
+
 class SupportRequestContent extends Component {
 
     render() {
@@ -10,7 +12,7 @@ class SupportRequestContent extends Component {
                 <ul>
                     {
                         this.props.supportRequests.map((object, index) => {
-                            return <div key={index}>{object.title}</div>
+                            return <SupportRequestItem {...object} key={index}/>
                         })
                     }
                 </ul>
---
+++ src/components/supportRequest/supportRequest_item.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class SupportRequestItem extends Component {
+
+    render() {
+        return (
+            <li>
+                <div>
+                    icon
+                </div>
+                <div>
+                    <div>
+                        <div>{this.props.title}</div>
+                        <div>{this.props.tenant} - {this.props.unit}</div>
+                    </div>
+                    <div>
+                        arrow
+                    </div>
+                </div>
+                <div>
+                    action
+                </div>
+                <div>
+                    {this.props.date}
+                </div>
+            </li>            
+        );
+    }
+}
+
+export default SupportRequestItem;
